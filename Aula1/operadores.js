96fbc05d09191ee2a7f37fc58b0dc65d0ebd226e
Nicoli <nicoli.rodriguesslopes@gmail.com>
2023-07-03 13:58:10 -0300

    Studying JS

---
+++ Aula1/operadores.js
@@ -0,0 +1,32 @@
+const num1 = 5;
+const num2 = 8;
+const num3 = 4.5;
+
+/*Operadores Aritiméticos
+    + adição
+    - subtração
+    * multiplicação
+    / divisão
+    ** exponenciação
+    % módulo-resto
+*/
+
+let result;
+
+result = num1 + num2;
+console.log(result)
+
+result = num1 - num3;
+console.log(result)
+
+result = num2 / num3;
+console.log(result)
+
+result = num1 * num2;
+console.log(result)
+
+result = num2 ** num3;
+console.log(result)
+
+result = num2 % num3
+console.log(result)
---
+++ Aula1/tipagem.js
@@ -0,0 +1,26 @@
+let variavel = 18;//number
+console.log(typeof variavel);
+
+variavel = 'Nick Lopes';//string
+console.log(typeof variavel);
+
+variavel = true;//boolean
+console.log(typeof variavel);
+
+variavel = {nome:'Nick Lopes', idade:18};//object
+console.log(typeof variavel);
+
+variavel = [1,2,3,4,5];//object
+console.log(typeof variavel);
+
+variavel = function() {return 1}//função
+console.log(typeof variavel);
+
+variavel = undefined;//indefinido
+console.log(variavel);
+
+variavel = null;//nulo-object
+console.log(variavel);
+
+variavel = NaN;//Not a Number - "contas erradas 0/0"-number
+console.log(variavel);
