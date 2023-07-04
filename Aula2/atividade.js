95ec6259ed4f9cc99a7cee33c8e4568f1cc7ecce
Nicoli <nicoli.rodriguesslopes@gmail.com>
2023-07-04 12:08:20 -0300

    Aula2 de JS

---
+++ Aula2/atividade.js
@@ -0,0 +1,22 @@
+//verifique se um número é par ou ímpar
+let n1;
+n1 = 5;
+n1 %= 2;
+n1=0 ? "par" : "impar"
+console.log(n1)
+
+//média aritmética de três números
+let nota1 = 5;
+let nota2 = 4;
+let nota3 = 6;
+let result;
+result = ((nota1 + nota2 + nota3) / 3) 
+console.log(result)
+
+//determine se um número é divisível por 3 e por 5 ao mesmo tempo
+let num;
+num = 15;
+(num %= 3 ) && (num %= 5 ) 
+console.log(num)
+
+//área de um triângulo com base e altura
